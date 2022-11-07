3e8410bd0400bf4ff3c5c796e8aad9c133ed2bee
samuelmalik <samuelmalik9@gmail.com>
2022-11-07 19:54:42 +0100

    Add project files.

---
+++ Open-Lab-11.00/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Open_Lab_11._00
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
